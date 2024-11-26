347f14cd21bf2aa6a7a85fa97ec74d55e954efd1
YehoGuy <yehowork23@gmail.com>
2024-11-27 00:02:15 +0200

    add their first test

--- grid_robot/main.py
+++ grid_robot/main.py
@@ -3,11 +3,11 @@ from heuristics import *
 from search import search
 
 if __name__ == '__main__':
-    map =  [[0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, -1, 0, 0, 0, 0], [0, 0, 2, 0, 0, 0, -1, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, -1, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 2, 0, 0], [0, 0, 0, 0, 0, 0, 0, 3]]
+    map = [[0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, -1, 0, 0, 0, 0], [0, 0, 2, 0, 0, 0, -1, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, -1, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 2, 0, 0], [0, 0, 0, 0, 0, 0, 0, 3]]
     #map[Y][X]
     robot_start_location = (7, 0)
     lamp_h = 3
-    lamp_location = (0,7)
+    lamp_location = (0, 7)
 
     start_state = grid_robot_state(map=map, robot_location=robot_start_location, lamp_height=lamp_h,
                                    lamp_location=lamp_location)
@@ -20,5 +20,5 @@ if __name__ == '__main__':
     # solution cost
     print(search_result[-1].g)
     for node in search_result:
-        print(node.state.robot_location)
+        print(base_heuristic(node.state))
         
